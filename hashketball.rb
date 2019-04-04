9ea40afec8f92a5db5628367e820ff489144a0ed
active-object-6703 <nerajno@gmail.com>
2019-04-04 23:00:13 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -117,28 +117,11 @@ def game_hash
    }
  end
 
- def good_practices
-  game_hash.each do |location, team_data|
-    #are you ABSOLUTELY SURE what 'location' and 'team data' are? use binding.pry to find out!
-    binding.pry
-      team_data.each do |attribute, data|
-        #are you ABSOLUTELY SURE what 'attribute' and 'team data' are? use binding.pry to find out!
-        binding.pry
-
-        #what is 'data' at each loop through out .each block? when will the following line of code work and when will it break?
-        data.each do |data_item|
-      end
-    end
+ def num_points_scored(player_name)
+     game_hash.each do |team, team_data|
+       team_data[:players].each do |player_name, player_data|
+         binding.pry
+         puts [player_data][:points].value
+     end
+     end
   end
-end
-
- # def num_points_scored(player_name)
- #    game_hash.each do |location|
- #     #  puts team_data[:players][:points]
- #     location.each do |team_name, player_stats|
- #        # diggy = [:players][player_name][:points]
- #        binding.pry
- #        puts [player_stats][player_name][:points]
- #     end
- #    end
- # end
