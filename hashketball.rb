3155bf824d236671041b89bb78587a999408dadf
active-object-6703 <nerajno@gmail.com>
2019-04-07 01:11:09 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -144,20 +144,59 @@ end
 
 def team_colors(team_name)
   game_hash.each do |team, team_data|
-  #  binding.pry
    if team_data[:team_name] == team_name
      return team_data[:colors]
   end
 end
 end
 
-def team_name #(game_hash) => optional
+def team_names #(game_hash) => optional
+  rtnd_team_names = []
   game_hash.each do |team, team_data|
-    rtnd_team_name = []
-    binding.pry
-    team_data.each do |team_keys, team_vals|
-      binding.pry
-      puts team_vals
-    end
+    rtnd_team_names += [team_data[:team_name]]
+  end
+  return rtnd_team_names
+end
+
+
+def player_numbers(team_name)
+   jersey_numbers = []
+   game_hash.each do |team, team_data|
+     if team_data[:team_name] == team_name
+     team_data[:players].each do |this_player, player_data|
+       jersey_numbers += [player_data[:number]]
+     end
+    #iterate through each team
+    #iterate through each place
+    # collect their #s
    end
+ end
+   return jersey_numbers
+end
+
+def player_stats(player_name)
+  game_hash.each do |team, team_data|
+    team_data[:players].each do |this_player, player_data|
+         if this_player == player_name
+           return player_data
+     end
+  end
+end
+end
+
+
+def big_shoe_rebounds
+  largest_shoe_size = 0
+  rebounds = 0
+  game_hash.each do |team, team_data|
+    #iterate through each player
+  team_data[:players].each do |this_player, player_data|
+      if player_data[:shoe] > largest_shoe_size
+        rebounds = player_data[:rebounds]
+        largest_shoe_size = player_data[:shoe]
+    end
+  end
+end
+# binding.pry
+return rebounds
 end
